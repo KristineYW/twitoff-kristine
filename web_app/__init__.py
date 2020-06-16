2b3b6546ccea9cbe98921af852ac8be6fba1abcc
Kristine <ying.kristine.wang@gmail.com>
2020-06-16 18:57:19 -0400

    added twitter files

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+from web_app.routes.twitter_routes import twitter_routes
 
-
-DATABASE_URL = "sqlite:///twitoff_kristine_development.db"  # relative filepath
+DATABASE_URL = "sqlite:///twitoff_kristine_development.db"
+# using relative filepath
 
 
 def create_app():
@@ -17,9 +19,10 @@ def create_app():
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+    app.register_blueprint(twitter_routes)
     return app
 
 
 if __name__ == "__main__":
     my_app = create_app()
-    my_app.run(debug=True)  # web_app/__init__.py
+    my_app.run(debug=True)
