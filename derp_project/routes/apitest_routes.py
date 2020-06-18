bd6347eb5e3414f35824bbf071a64d1e686d8ea0
Kristine <ying.kristine.wang@gmail.com>
2020-06-18 12:42:24 -0400

    deleted example folder

--- derp_project/routes/apitest_routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, request
-
-apitest_routes = Blueprint("apitest_routes", __name__)
-
-@apitest_routes.route('/retrieve')
-def our_home():
-
-    return render_template("api_results.html", title="derp_proj")
